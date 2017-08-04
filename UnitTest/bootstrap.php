dc495e6fab186eb38c090abee69af48f635d3281
Baraban35 <annie.baraban@gmail.com>
2017-08-04 21:48:58 +0200

    Ajustement des fichiers de config. de PHPUnit.
    Fichier xml complété, réécriture du fichier bootstrap.php pour que toutes
    les classes PHP soient chargées avant que les classes de test n'utilisent
    ces dernières, création d'une classe Autoloader.

--- UnitTest/bootstrap.php
+++ UnitTest/bootstrap.php
@@ -1,9 +1,7 @@
 <?php
-function loader($class)
-{
-    $file = $class . '.php';
-    if (file_exists($file)) {
-        require $file;
-    }
-}
-spl_autoload_register('loader');
+include_once('AutoLoader.php');
+// Register the directory to your include files
+// la constante __DIR__ contient le nom de répertoire du fichier courant
+// dirname(__DIR__) fournit le répertoire parent du répertoire courant
+// ceci pour remonter au répertoire racine du projet
+AutoLoader::registerDirectory(dirname(__DIR__) . '/classes');
